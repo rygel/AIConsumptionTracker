c1d3fef8054e29f656eeceff0eba531d68e4bf8b
Alexander Brandt <alexander.brandt@outerstellar.de>
2026-03-03 14:50:36 +0200

    refactor: Move Web DTOs to AIUsageTracker.Web.Models namespace
    - Create AIUsageTracker.Web.Models namespace
    - Move ProviderInfo, UsageSummary, ChartDataPoint from WebDatabaseService
    - Update WebDatabaseService to use new namespace

--- AIUsageTracker.Web/Services/WebDatabaseService.cs
+++ AIUsageTracker.Web/Services/WebDatabaseService.cs
@@ -2,6 +2,7 @@ using Dapper;
 using Microsoft.Data.Sqlite;
 using AIUsageTracker.Core.Models;
 using AIUsageTracker.Infrastructure.Providers;
+using AIUsageTracker.Web.Models;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
@@ -1091,32 +1092,5 @@ public class WebDatabaseService
     }
 }
 
-public class ProviderInfo
-{
-    public string ProviderId { get; set; } = string.Empty;
-    public string ProviderName { get; set; } = string.Empty;
-    public string PlanType { get; set; } = string.Empty;
-    public bool IsActive { get; set; }
-    public string? AuthSource { get; set; }
-    public string? AccountName { get; set; }
-    public double LatestUsage { get; set; }
-    public DateTime? NextResetTime { get; set; }
-}
-
-public class UsageSummary
-{
-    public int ProviderCount { get; set; }
-    public double AverageUsage { get; set; }
-    public string? LastUpdate { get; set; }
-}
-
-public class ChartDataPoint
-{
-    public string ProviderId { get; set; } = string.Empty;
-    public string ProviderName { get; set; } = string.Empty;
-    public DateTime Timestamp { get; set; }
-    public double RequestsPercentage { get; set; }
-    public double RequestsUsed { get; set; }
-}
 
 
